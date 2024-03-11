2ee0007b17ee26912f2a1e65c8423c8aea06ecf2
devarim <wachirabriangeita@gmail.com>
2024-03-11 12:43:17 +0300

    help

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,6 @@ from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
-
+"""
+Instantiate FileStorage and reload data from the storage file.
+"""
--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+"""
+BaseModel class module.
+"""
 import uuid
 from datetime import datetime
 from models import storage
 
 
 class BaseModel:
+    """
+    BaseModel class for other classes to inherit.
+    """
+
     def __init__(self, *args, **kwargs):
+        """Initialize BaseModel instance."""
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
@@ -13,8 +21,10 @@ class BaseModel:
                 if key == 'created_at' or key == 'updated_at':
                     if isinstance(value, str):
                         setattr(self, key,
-                                datetime.strptime
-                                (value, '%Y-%m-%dT%H:%M:%S.%f'))
+                                datetime.strptime(
+                                                  value,
+                                                  '%Y-%m-%dT%H:%M:%S.%f'
+                                                  ))
                     else:
                         setattr(self, key, value)
                 else:
@@ -25,10 +35,12 @@ class BaseModel:
             storage.new(self)
 
     def save(self):
+        """Update the updated_at attribute and save to storage."""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
+        """Return a dictionary representation of the instance."""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
@@ -36,12 +48,13 @@ class BaseModel:
         return obj_dict
 
     def __str__(self):
+        """Return a string representation of the instance."""
         return "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__
         )
 
     @classmethod
     def all(cls):
-        """Returns a dictionary of all instances of the class."""
+        """Return a dictionary of all instances of the class."""
         from models import storage
         return storage.all(cls)
